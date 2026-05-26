b6db4c47b5d2bbfc6b27589e80b94b235b84d7f2
Yael <243678@ids.upchiapas.edu.mx>
2026-05-26 17:07:02 -0600

    lualine más estilizado, tiene un gatito

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,3 +1,15 @@
-vim.pack.add({"https://github.com/nvim-lualine/lualine.nvim"})
+vim.pack.add({ "https://github.com/nvim-lualine/lualine.nvim" })
 
-require("lualine").setup()
+require("lualine").setup({
+	options = {
+		theme = 'auto',
+		section_separators = { left = '', right = ''},
+		component_separators = { left='', right='' }
+	},
+	sections = {
+		lualine_c = { 'filename', 'lsp_status' },
+		lualine_z = { 'location', '""' }
+	}
+})
+--local lol = require('lualine').get_config()
+--print(vim.inspect(lol))
