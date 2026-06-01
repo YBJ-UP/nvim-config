d1dda5a45a1026e33406c55fa877dc68ebdc7c16
Yael <243678@ids.upchiapas.edu.mx>
2026-05-31 18:53:00 -0600

    cambios a nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,6 +2,16 @@ vim.pack.add({ "https://github.com/nvim-tree/nvim-tree.lua" })
 
 local opts = {
 	reload_on_bufenter = true,
+	auto_reload_on_write = true,
+	disable_netrw = true,
+	hijack_netrw = true,
+	hijack_unnamed_buffer_when_opening = true,
+	respect_buf_cwd = false,
+	view = {
+		centralize_selection = true,
+		side = "right",
+		width = 50
+	},
 	diagnostics = {
 		enable = true
 	},
@@ -10,6 +20,11 @@ local opts = {
 	},
 	renderer = {
 		icons = {
+			web_devicons = {
+				folder = {
+					enable = true
+				}
+			},
 			glyphs = {
 				git = {
 					unstaged = "~",
