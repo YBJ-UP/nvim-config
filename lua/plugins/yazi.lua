5a9a7b6b657fe6f96d10eaa1169d08d370cde746
Yael <243678@ids.upchiapas.edu.mx>
2026-06-04 20:54:40 -0600

    refactor: Eliminación del plugin de yazi debido a su desuso

--- lua/plugins/yazi.lua
+++
@@ -1,8 +0,0 @@
--- SE DEBE DE TENER YAZI INSTALADO EN EL SISTEMA PARA USAR ESTE PLUGIN
--- No sé si está disponible en Windows
--- Si estás usando NeoVim en Windows ya pásate a Linux
-require("plugins.plenary")
-
-vim.pack.add({"https://github.com/mikavilpas/yazi.nvim"})
-
--- vim.keymap.set("n", "<leader>e", ":Yazi<CR>", { desc = "Open file explorer" })
