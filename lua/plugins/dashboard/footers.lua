c78d160e0cb38118947087f0c11b5113a07be0f3
Yael <243678@ids.upchiapas.edu.mx>
2026-06-08 16:10:08 -0600

    refactor: cambio de la lógica para obtener la fecha y versión

--- lua/plugins/dashboard/footers.lua
+++ lua/plugins/dashboard/footers.lua
@@ -1,14 +1,19 @@
-local function info_footer()
+local function date_footer()
 	local date = os.date("%d/%m/%Y - %H:%M:%S")
+	return "󰃭 " .. date
+end
+
+local function version_footer()
 	local version = vim.version()
 	local version_info = version.major .. "." .. version.minor .. "." .. version.patch
-	local plugins = require("core.plugin_manager").get_active_packs()
 
-	return {
-		"󰃭 " .. date,
-		" " .. version_info,
-		--plugins
-	}
+	return " " .. version_info
+end
+
+local function info_footer()
+	local plugins = require("core.plugin_manager").get_active_packs() -- da mal el número xd
+
+	return date_footer() .. "\t" .. version_footer()
 end
 
 local footers = {
@@ -33,7 +38,9 @@ local footers = {
 	experienciapromedio = {
 		"Paso más tiempo configurando neovim que programando"
 	},
-	info = info_footer()
+	info = {
+		info_footer()
+	}
 }
 
 return footers
