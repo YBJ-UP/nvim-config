92fd26e66a1c731a8cf503766fedef32e121a36c
Yael <243678@ids.upchiapas.edu.mx>
2026-06-19 01:56:14 -0600

    style: descripción de atajos

--- lua/core/buffers.lua
+++ lua/core/buffers.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<C-i>", ":bn<CR>")
-vim.keymap.set("n", "<S-Tab>", ":bp<CR>")
-vim.keymap.set("n", "<C-A-i>", ":%bd!|e#|bd#<CR>") -- Cierra todos los buffers excepto el que esté activo
-vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format) -- Aplica formato según el LSP activo, si no hay ninguno pues no funciona
+vim.keymap.set("n", "<C-i>", ":bn<CR>", { desc = "Siguiente búfer" })
+vim.keymap.set("n", "<S-Tab>", ":bp<CR>", { desc = "Búfer anterior" })
+vim.keymap.set("n", "<C-A-i>", ":%bd!|e#|bd#<CR>", { desc = "Cerrar todos los búfers excepto el activo" }) -- Cierra todos los buffers excepto el que esté activo
+vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { desc = "Aplicar formato según el LSP" }) -- Aplica formato según el LSP activo, si no hay ninguno pues no funciona
